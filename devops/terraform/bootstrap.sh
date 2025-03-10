feb8b08668d4de501c75fbea4679da55189fe0d8
Ron Shakutai <ronshakutai@microsoft.com>
2025-03-10 14:45:46 +0000

    fix: add a comment to clarify the role assignment process in bootstrap.sh

--- devops/terraform/bootstrap.sh
+++ devops/terraform/bootstrap.sh
@@ -73,7 +73,7 @@ fi
 
 # shellcheck disable=SC1091
 source ../scripts/mgmtstorage_enable_public_access.sh
-
+# check.
 # Grant user blob data contributor permissions
 echo -e "\n\e[34m»»» 🔑 \e[96mGranting Storage Blob Data Contributor role to the current user\e[0m..."
 if [ -n "${ARM_CLIENT_ID:-}" ]; then
